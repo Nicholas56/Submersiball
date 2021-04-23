7ee4554e7e8747a2f89564c7eb44fd63601648cf
HIN16000832 <45455801+HindleyL@users.noreply.github.com>
2021-04-23 15:24:21 +0100

    Main Menu + Sounds

---
+++ Submersiball/Assets/Scripts/Scene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public static int currentScene;
+
+    public void GoToLevel(int Level)
+    {
+        Time.timeScale = 1;
+        currentScene = Level;
+        SceneManager.LoadScene(Level);
+    }
+
+    public void ReturnToLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void AddScene(int level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
