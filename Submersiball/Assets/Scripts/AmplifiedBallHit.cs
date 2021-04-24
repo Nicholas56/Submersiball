ae96b8576b05e6a98babb92b16b06c072daead6f
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-04-24 12:30:50 +0100

    Made the game more responsive
    Made the input buttons more responsive.
    Added force multiplier to ball
    Added lights to subs

---
+++ Submersiball/Assets/Scripts/AmplifiedBallHit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmplifiedBallHit : MonoBehaviour
+{
+    [SerializeField] float forceMultiplier;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(collision.GetContact(0).normal * forceMultiplier, ForceMode.Impulse);
+    }
+}
--- Submersiball/Assets/Scripts/SubmarineMovement.cs
+++ Submersiball/Assets/Scripts/SubmarineMovement.cs
@@ -6,14 +6,16 @@ public class SubmarineMovement : MonoBehaviour
 {
     Rigidbody rb;
 
+    [Header("Movement Controls")]
+    [SerializeField] KeyCode accelerateButton;
+    [SerializeField] KeyCode boostButton;
+
     [Header("Movement Values")]
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float boostSpeed = 2f;
     [SerializeField] float boostTime = 2;
     [SerializeField] float maxBoostTime = 2;
 
-    [SerializeField] float maxVerticalRot = 75;
-    [SerializeField] float minVerticalRot = -75;
     [SerializeField] bool boost = true;
     [SerializeField] bool accel = false;
 
@@ -24,27 +26,28 @@ public class SubmarineMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    void FixedUpdate()
+    private void Update()
     {
-        //Mouse click to boost, if boost is available
-        if (Input.GetMouseButton(0) && boostTime>0 && boost)
+        //Key to boost, if boost is available
+        if (Input.GetKey(boostButton) && boostTime>0 && boost)
         {
-            rb.AddForce(transform.forward * boostSpeed,ForceMode.VelocityChange);
+            rb.AddForce(transform.forward * boostSpeed,ForceMode.Acceleration);
             boostTime = Mathf.Max(boostTime - Time.deltaTime, 0);
-            if (boostTime == 0) { boost = false; }
+            if (boostTime <= 0.2f) { boost = false; }
         }
         else { boostTime = Mathf.Min(boostTime + Time.deltaTime, maxBoostTime); if (boostTime == maxBoostTime) boost = true; }
 
-        //Spacebar to initiate acceleration
-        if (Input.GetKeyDown(KeyCode.Space)){accel = !accel;}
-        if (accel){rb.velocity = transform.forward * moveSpeed; } else { rb.velocity = Vector3.zero; }
-
+        //Key to initiate acceleration
+        if (Input.GetKeyDown(accelerateButton)) { accel = !accel; }
+        if (accel) { rb.AddForce(transform.forward * moveSpeed,ForceMode.Force); } //else { rb.velocity = Vector3.zero; }
+    }
+    void FixedUpdate()
+    {
         //Takes the two dimensional inputs to calculate direction to move in
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
         transform.Rotate(new Vector3(v,h));
         //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + v, transform.localEulerAngles.y + h, transform.localEulerAngles.z);
-        Debug.Log(transform.localEulerAngles.x);
     }
 }
