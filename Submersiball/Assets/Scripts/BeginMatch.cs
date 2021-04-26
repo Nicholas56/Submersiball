63af13b3a704f4f93b7509fbae50efdff89ee496
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-04-26 12:20:57 +0100

    Respawn and pause
    Added respawning (to a point)
    Added rudimentary pause screen

---
+++ Submersiball/Assets/Scripts/BeginMatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginMatch : MonoBehaviour
+{
+    [SerializeField] KeyCode pauseKey;
+    GameObject pauseButton;
+    private void Start()
+    {
+        Time.timeScale = 0;
+        pauseButton = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            pauseButton.SetActive(true);
+        }
+    }
+
+    public void BeginGame()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseButton.SetActive(false);
+    }
+}
--- Submersiball/Assets/Scripts/Explode.cs
+++ Submersiball/Assets/Scripts/Explode.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class Explode : MonoBehaviour
 {
     [SerializeField] ParticleSystem ps;
+    [SerializeField] Vector3 respawnPoint;
+    Quaternion rot;
+    private void Start()
+    {
+        rot = transform.rotation;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "Player") { ps.Play(); }
+        if (collision.transform.tag == "Player") { ps.Play();StartCoroutine("Respawn"); }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = respawnPoint;
+        transform.rotation = rot;
     }
 }
