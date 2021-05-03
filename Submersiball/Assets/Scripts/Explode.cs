bc431c0bbccbd1be3dc7652dfe22a7107ec05cb7
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-05-03 19:09:09 +0100

    Made demo match for menu scene
    Added textures to the maps (red & green only)
    Imported Mirror
    Made a demo game for Main Menu
    Fixed Explosions (slower sub explodes)

--- Submersiball/Assets/Scripts/Explode.cs
+++ Submersiball/Assets/Scripts/Explode.cs
@@ -6,15 +6,27 @@ public class Explode : MonoBehaviour
 {
     [SerializeField] ParticleSystem ps;
     public Vector3 respawnPoint;
+    public float speedDifForExplosion = 10;
     Quaternion rot;
+    Rigidbody rb;
     private void Start()
     {
         rot = transform.rotation;
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "Player") { ps.Play();StartCoroutine("Respawn"); }
+        if (collision.transform.tag == "Player") 
+        {
+            float velo1 = rb.velocity.magnitude;
+            float velo2 = collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            if (velo2 > velo1 + speedDifForExplosion)
+            {
+                ps.Play();
+                StartCoroutine("Respawn");
+            }
+        }
     }
 
     IEnumerator Respawn()
