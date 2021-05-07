737f95054fb0892391d9b8294b55980b537c28c6
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-05-07 12:41:13 +0100

    Network players Camera
    Added virtual camera control to network players
    Added Cinemachine package

---
+++ Submersiball/Assets/Scripts/Network/PlayerCameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Cinemachine;
+
+public class PlayerCameraController : NetworkBehaviour
+{
+    [Header("Camera")]
+    [SerializeField] Vector2 maxFollowOffset = new Vector2(-1f, 6f);
+    [SerializeField] Vector2 cameraVelocity = new Vector2(4f,0.25f);
+    [SerializeField] Transform playerTransform = null;
+    [SerializeField] CinemachineVirtualCamera virtualCamera = null;
+
+    PlayerControls controls;
+    PlayerControls Controls
+    {
+        get
+        {
+            if (controls != null) { return controls; }
+            return controls = new PlayerControls();
+        }
+    }
+    CinemachineTransposer transposer;
+
+    public override void OnStartAuthority()
+    {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        virtualCamera.gameObject.SetActive(true);
+        enabled = true;
+
+        Controls.Gameplay.Look.performed += ctx => Look(ctx.ReadValue<Vector2>());
+    }
+
+    [ClientCallback]
+    private void OnEnable() => Controls.Enable();
+    [ClientCallback]
+    private void OnDisable() => Controls.Disable();
+
+    void Look(Vector2 lookAxis)
+    {
+        float deltaTime = Time.deltaTime;
+        float followOffset = Mathf.Clamp(
+            transposer.m_FollowOffset.y - (lookAxis.y * cameraVelocity.y * deltaTime),
+            maxFollowOffset.x,
+            maxFollowOffset.y);
+        transposer.m_FollowOffset.y = followOffset;
+        playerTransform.Rotate(0f, lookAxis.x * cameraVelocity.x * deltaTime, 0f);
+    }
+}
