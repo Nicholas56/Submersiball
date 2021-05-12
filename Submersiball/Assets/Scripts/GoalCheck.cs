c8a95955f88571eb204aecf8b9bf7ff03a70be13
ernest68680 <44022033+ernest68680@users.noreply.github.com>
2021-05-12 16:23:04 +0100

    UI Updated

--- Submersiball/Assets/Scripts/GoalCheck.cs
+++ Submersiball/Assets/Scripts/GoalCheck.cs
@@ -6,6 +6,13 @@ public class GoalCheck : MonoBehaviour
 {
     [SerializeField] ParticleSystem ps;
 
+    UI_Manager uiMan;
+
+    private void Start()
+    {
+        uiMan = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UI_Manager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Ball")
@@ -13,6 +20,26 @@ public class GoalCheck : MonoBehaviour
             other.transform.position = Vector3.zero;
             other.GetComponent<Rigidbody>().velocity = Vector3.zero;
             ps.Play();
+
+            if(tag == "GoalLeft")
+            {
+                ScoreLeft();
+            }
+            else if (tag == "GoalRight")
+            {
+                ScoreRight();
+            }
         }
+    }
+
+    private void ScoreLeft()
+    {
+        uiMan.ScoreTeamOne();
+    }
+
+    private void ScoreRight()
+    {
+        uiMan.ScoreTeamTwo();
+
     }
 }
