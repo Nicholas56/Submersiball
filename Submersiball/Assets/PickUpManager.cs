2baee5b0102ee568737f9895c0be11ebebf58ab8
ernest68680 <44022033+ernest68680@users.noreply.github.com>
2021-05-16 19:56:12 +0100

    PickUps Updated

---
+++ Submersiball/Assets/PickUpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpManager : MonoBehaviour
+{
+    public static PickUpManager current;
+
+    [SerializeField] GameObject[] boostPickupLocations;
+
+    [SerializeField]
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    private void Start()
+    {
+        boostPickupLocations = GameObject.FindGameObjectsWithTag("BoostLocation");
+    }
+
+    public void SpawnBoostPickup(Transform spawnLocation, GameObject boostPickup)
+    {
+        Instantiate(boostPickup, spawnLocation);
+    }
+}
+    
--- Submersiball/Assets/Scripts/PickUps/BoostPickUp.cs
+++ Submersiball/Assets/Scripts/PickUps/BoostPickUp.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class BoostPickUp : MonoBehaviour
 {
+    MeshRenderer meshRen;
+
+    SphereCollider SphereCol;
+
+    Animator anim;
+
+    private void Start()
+    {
+        meshRen = gameObject.GetComponent<MeshRenderer>();
+
+        SphereCol = gameObject.GetComponent<SphereCollider>();
+
+        anim = gameObject.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             other.GetComponent<SubmarineControl>().StartBoostRefill();
+
+            meshRen.enabled = false;
+
+            SphereCol.enabled = false;
+
+            StartCoroutine("RespawnBoostPickUp", 5);
+
+            anim.SetBool("Grow", false);
         }
+    }
+
+    private IEnumerator RespawnBoostPickUp(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        meshRen.enabled = true;
+
+        SphereCol.enabled = true;
+
+        anim.SetBool("Grow", true);
     }
 }
