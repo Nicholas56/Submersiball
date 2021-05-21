c7d2ef7bd332286aa318f15d8d83b7380f783f86
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2021-05-21 15:21:14 +0100

    Underwater city?
    Added background props

---
+++ Submersiball/Assets/Scripts/RandomBobbing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBobbing : MonoBehaviour
+{
+    float bobRange = 0;
+    float bobSpeed = 0.2f;
+    Vector3 initialPos;
+
+    private void Awake()
+    {
+        initialPos = transform.position;
+        bobRange = Random.Range(0.5f, 10f);
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > initialPos.y + bobRange)
+        {
+            bobSpeed = -bobSpeed;
+        }
+        else if (transform.position.y < initialPos.y - bobRange)
+        {
+            bobSpeed = -bobSpeed;
+        }
+        transform.position = new Vector3(initialPos.x, transform.position.y + bobSpeed * Time.deltaTime, initialPos.z);
+    }
+}
