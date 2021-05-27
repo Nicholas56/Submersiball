c9a240e2a1c8c932036d3c400905f46a6747fae7
ernest68680 <44022033+ernest68680@users.noreply.github.com>
2021-05-27 10:02:33 +0100

    Finished Pickup Manager

--- Submersiball/Assets/PickUpManager.cs
+++ Submersiball/Assets/PickUpManager.cs
@@ -6,11 +6,13 @@ public class PickUpManager : MonoBehaviour
 {
     public static PickUpManager current;
 
-    GameObject[] allPickupLocations;
-    GameObject[] freePickupLocations;
-    GameObject[] takenPickupLocations;
+    [SerializeField] List<GameObject> allPickupLocations;
+    List<GameObject> freePickupLocations;
+    List<GameObject> takenPickupLocations;
 
-    [SerializeField] GameObject[] pickups;
+    [SerializeField] List<GameObject> pickups;
+
+    [SerializeField] int spawnCoolDown;
 
     private void Awake()
     {
@@ -19,9 +21,53 @@ public class PickUpManager : MonoBehaviour
 
     private void Start()
     {
-        allPickupLocations = GameObject.FindGameObjectsWithTag("PickupLocation");
-
         freePickupLocations = allPickupLocations;
+
+        StartCoroutine("SpawnPickupTimer");
+    }
+
+    IEnumerator SpawnPickupTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnCoolDown);
+
+            if (freePickupLocations.Count != 0)
+            {
+                if (pickups.Count != 0)
+                {
+                    SpawnPickUp();
+                }
+                else
+                {
+                    Debug.LogError("There are no pickups available on the list");
+                }
+            }
+            else
+            {
+                Debug.LogError("There are no spawn locations available on the list");
+            }
+        }
+    }
+
+    void SpawnPickUp()
+    {
+        int randomLocationIndex = Random.Range(1, freePickupLocations.Count);
+
+        int randomPickUpIndex = Random.Range(1, pickups.Count);
+
+        Instantiate(pickups[randomPickUpIndex], freePickupLocations[randomPickUpIndex].transform);
+
+        takenPickupLocations.Add(freePickupLocations[randomLocationIndex]);
+
+        freePickupLocations.Remove(freePickupLocations[randomLocationIndex]);
+    }
+
+    public void FreeUpSpawnLocation(GameObject location)
+    {
+        takenPickupLocations.Remove(location);
+
+        freePickupLocations.Add(location);
     }
 }
     
